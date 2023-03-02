3e4877cb9359a3d152923487f613e77ba7fb9910
Flávia Correia <fllaviacorreia@outlook.com>
2023-03-02 10:30:33 -0300

    feat: alter home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,52 +1,18 @@
 import React from 'react';
+import Header from '@/components/header';
 
 import './style.css';
-
-// eslint-disable-next-line import/no-unresolved, import/extensions
-
 export default function Home() {
   return (
-    <>
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div
-              id="search-spinner"
-              aria-hidden
-              hidden={true}
-            />
-            <div
-              className="sr-only"
-              aria-live="polite"
-            ></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/dashboard`}>Dashboard</a>
-            </li>
-            <li>
-              <a href={`/new`}>Novo funcionário</a>
-            </li>
-            <li>
-              <a href={`/list`}>Listar funcionários</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div id="detail"></div>
-    </>
-  );
+    <section className="containerHome">
+      <Header />
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+home
+    </section>
+  )
 }
