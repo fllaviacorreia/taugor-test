f9d604be63b3978cc07920da29de635c5142b1fb
Flávia Correia <fllaviacorreia@outlook.com>
2023-03-02 10:26:09 -0300

    alter configs

---
+++ src/config/auth.js
@@ -0,0 +1,22 @@
+export const TOKEN_KEY = "@taugor-token";
+export const USER_KEY = "@taugor-user";
+
+export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+
+//dados não registrados no banco
+export const isRegistered = () => localStorage.getItem(USER_KEY) !== null;
+
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
+export const getUser = () => localStorage.getItem(USER_KEY);
+
+export const login = token => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+
+export const setUser = user => {
+  localStorage.setItem(USER_KEY, user);
+};
+
+export const logout = () => {
+  localStorage.removeItem(TOKEN_KEY);
+};
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,25 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
-// import { getAnalytics } from "firebase/analytics";
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MESASUREMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MESASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp =firebase.initializeApp(firebaseConfig);
 
-// const analytics = getAnalytics(app);
+const auth = firebase.auth();
+const db = firebaseApp.firestore();
+const storage = firebaseApp.storage();
+
+export { auth, db, storage };
