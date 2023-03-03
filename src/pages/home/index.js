da6cce00c5dbbbaf89017f2ef17193c05ddcdaa2
Flávia Correia <fllaviacorreia@outlook.com>
2023-03-02 22:16:20 -0300

    alter home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import Header from '@/components/header';
 
 import './style.css';
+// import { isRegistered } from '@/config/auth';
+// import registerUser from '../../services/registerData';
+
 export default function Home() {
+  // React.useEffect(() => {
+  //     if(isRegistered){
+  //       registerUser();
+  //     }
+  // }, [])
+
   return (
     <section className="containerHome">
       <Header />
+      <br />
+      <br />
+      <br />
 
-<br/>
-<br/>
-<br/>
-<br/>
-
-home
+      Olá, seja bem vindo!
     </section>
   )
 }
