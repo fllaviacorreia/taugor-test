8e4c2c059299f45c88bb5f732ea7523cc25e5703
Flávia Correia <fllaviacorreia@outlook.com>
2023-03-14 08:47:43 -0300

    feat: alter error page

--- src/pages/errorPage/index.js
+++ src/pages/errorPage/index.js
@@ -1,15 +1,24 @@
+import Button from '@/components/elements/button';
 import React from 'react';
-import { useRouteError } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+// import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
+  // const error = useRouteError();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/')
+  }
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>Desculpe, ocorreu algum erro.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i> URL não encontrada...</i>
+        <Button title="Ir para Home" typeButton={'input-button'} onClick={handleClick} />
+        {/* <i>{error.statusText || error.message}</i> */}
       </p>
     </div>
   );
