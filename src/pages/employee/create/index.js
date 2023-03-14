ea8c9469ebba1ac47a4fb03b2ff462197de29dfb
Flávia Correia <fllaviacorreia@outlook.com>
2023-03-14 08:44:46 -0300

    feat:  alter create  employee page

--- src/pages/employee/create/index.js
+++ src/pages/employee/create/index.js
@@ -1,230 +1,244 @@
 import React from "react";
-import Button from "@/components/elements/button";
 import { useNavigate } from "react-router-dom";
-import { Box, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
-import "./style.css";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-import registerDataAdmin from "@/services/registerDataAdmin";
+import registerDataAdmin from "@/services/create/registerDataAdmin";
+import defaultProfile from '@/assets/default-profile.png'
+import { getItem } from "@/config/auth";
+
+import SelectGeneral from "@/components/elements/input/select";
+import InputGeneral from "@/components/elements/input/input";
+import Button from "@/components/elements/button";
+
+import "../style.css";
+
 export default function CreateEmployee() {
     const navigate = useNavigate();
+    const atualUser = getItem() === 'administrador@taugor.teste.com';
+    const [gender, setGender] = React.useState(''); //guardar o sexo
+    const [levelAccess, setLevelAccess] = React.useState(''); //guardar o sexo
+    const [urlImage, setUrlImage] = React.useState(defaultProfile); //url para imagem visualizada
 
-    const initUser = {
-        id:'',
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        bornDate:'',
-        photo:'',
-        phone:'',
-        gender:'',
-        address:'',
-        office:'',
-        sector:'',
-        admissionDate:'',
-        salary:'',
-        createdAt:'',
-        levelAccess: 'minimun',
-    }
-    
-    const [user, setUser] = React.useState(initUser);
-
-    const [errorMessage, setErrorMessage] = React.useState('');
+    const [errorMessage, setErrorMessage] = React.useState(''); //mensagens de erro
 
     const handleChange = (event) => {
-        setUser({...user, [event.target.name]: event.target.value})
+        setGender(event.target.value);
+    };
+
+    const handleChange2 = (event) => {
+        setLevelAccess(event.target.value);
     };
 
     const setPhoto = (event) => {
         var file = event.target.files[0];
         var reader = new FileReader();
-        if (file && file.type.match('image.*')) {
-            reader.readAsDataURL(file);
-        } else {
-            setUser({...user, ['photo']:''});
-        }
-        reader.onloadend = function () {
-            setUser({...user, ['photo']:reader.result});
-        }
-
+        (file && file.type.match('image.*')) ? reader.readAsDataURL(file) : setUrlImage('');
+        reader.onloadend = function () { setUrlImage(reader.result); }
     }
-    
-    const handleSubmit = async (event) => registerDataAdmin(event, setErrorMessage, user, setUser, navigate);
 
-    const handleCancel = () => {
-        navigate("/employees");
-    };
+    const handleSubmit = async (event) =>
+        registerDataAdmin(event, setErrorMessage, handleRedirect);
+
+
+    function handleRedirect ( uri ) { navigate(uri) }
 
     return (
         <section className="containerFormGeneral">
             <div className="containerForm">
-                <Typography component="h2" variant="h6">
+                <h2 >
                     Cadastrar novo funcionário
-                </Typography>
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                    <Typography component="h3" variant="h6">
-                        Dados de contato
-                    </Typography>
-                    <TextField
-                        name="firstName"
-                        required
-                        fullWidth
-                        id="firstName"
-                        label="Nome"
-                        autoFocus
-                        value={user.firstName}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        fullWidth
-                        id="lastName"
-                        label="Sobrenome"
-                        name="lastName"
-                        value={user.lastName}
-                        onChange={handleChange}
-                    />
-                    <Grid2 container spacing={2} fullWidth>
-                        <img className='imageEmployee' alt='Foto do duncionário' src={user.photo} />
-                        <TextField
-                            required
-                            fullWidth
-                            name="photo"
-                            label="Foto do perfil"
-                            type="file"
-                            onChange={setPhoto}
-                            id="photo"
-                        />
-                    </Grid2>
+                </h2>
+                <form onSubmit={handleSubmit}>
+                    <h3> Dados pessoais </h3>
+                    <div className="containerRow">
+                        <div className="itemForm">
+                            <InputGeneral
+                                name="firstName"
+                                required
+                                classess={'general-input '}
+                                id="firstName"
+                                title="Nome"
+                                autoFocus
+                            />
+                        </div>
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input '}
+                                id="lastName"
+                                title="Sobrenome"
+                                name="lastName"
+                            />
+                        </div>
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input'}
+                                name="bornDate"
+                                title="Data de nascimento"
+                                type="date"
+                                id="bornDate"
+                            />
+                        </div>
 
-                    <TextField
-                        required
-                        fullWidth
-                        name="address"
-                        label="Endereço"
-                        type="text"
-                        id="address"
-                        value={user.address}
-                        onChange={handleChange}
-                    />
-                    <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">Sexo</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"                            
-                            value={user.gender}
-                            onChange={handleChange}
-                            label="Sexo"
-                        >
-                            <MenuItem value={'female'}>Feminino</MenuItem>
-                            <MenuItem value={'male'}>Masculino</MenuItem>
-                        </Select>
-                    </FormControl>
 
-                    <TextField
-                        required
-                        fullWidth
-                        id="phone"
-                        label="Telefone"
-                        name="phone"
-                        type="tel"
-                        value={user.phone}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        fullWidth
-                        name="bornDate"
-                        label="Data de nascimento"
-                        type="date"
-                        id="bornDate"
-                        value={user.bornDate}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        fullWidth
-                        id="email"
-                        label="E-mail"
-                        name="email"
-                        type="email"
-                        value={user.email}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        fullWidth
-                        name="password"
-                        label="Senha"
-                        type="password"
-                        id="password"
-                        value={user.password}
-                        onChange={handleChange}
-                    />
-                    <Typography component="h3" variant="h6">
+                        <div className="itemForm">
+                            <SelectGeneral
+                                required
+                                classess={'general-input '}
+                                name="gender"
+                                value={gender}
+                                title="Sexo"
+                                onChange={handleChange}
+                                items={[{ value: 'female', name: 'Feminino' }, { value: 'male', name: 'Masculino' }]}
+                                id="gender" />
+                        </div>
+
+
+                        <div className="itemForm containerInputPhoto">
+                            <img className='imageEmployee' alt='Foto do duncionário' src={urlImage} />
+                            < InputGeneral
+                                classess={'general-input '}
+                                name="photo"
+                                title="Foto do perfil"
+                                type="file"
+                                onChange={setPhoto}
+                                id="photo"
+                            />
+                        </div>
+                    </div>
+                    <h3>
+                        Dados de acesso e contato
+                    </h3>
+
+                    <div className="containerRow">
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input '}
+                                id="phone"
+                                title="Telefone"
+                                name="phone"
+                                type="tel"
+                            />
+                        </div>
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input address-input'}
+                                name="address"
+                                title="Endereço"
+                                type="text"
+                                id="address"
+                            />
+                        </div>
+                    </div>
+                    <div className="containerRow">
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input address-input'}
+                                id="email"
+                                title="E-mail"
+                                name="email"
+                                type="email"
+                            />
+                        </div>
+
+                        {
+                            atualUser
+                                ? 
+                                    <div className="itemForm">
+                                        {<SelectGeneral
+                                            required
+                                            classess={'general-input select-size'}
+                                            name="levelAccess"
+                                            title="Nível de acesso"
+                                            value={levelAccess}
+                                            onChange={handleChange2}
+                                            items={[{ value: 'minimum', name: 'Baixo' }, { value: 'medium', name: 'Médio' }]}
+                                            id="levelAccess" />}
+                                    </div>
+                                : <></>
+
+                        }
+
+                        {
+                            atualUser && levelAccess === 'medium'
+                            ?
+                            <div className="itemForm">
+
+                                    <InputGeneral
+                                        required
+                                        classess={'general-input '}
+                                        name="password"
+                                        title="Senha"
+                                        type="password"
+                                        id="password"
+                                    /> </div>
+                                    :<></>
+                        }
+                    </div>
+                    <h3>
                         Dados do funcionário
-                    </Typography>
-                    <Grid container>
-                        <TextField
-                            required
-                            fullWidth
-                            name="office"
-                            label="Cargo"
-                            type="text"
-                            id="office"
-                            value={user.office}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            required
-                            fullWidth
-                            name="sector"
-                            label="Setor"
-                            type="text"
-                            id="sector"
-                            value={user.sector}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            required
-                            fullWidth
-                            name="admissionDate"
-                            label="Data de admissão"
-                            type="date"
-                            id="admissionDate"
-                            value={user.admissionDate}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            required
-                            fullWidth
-                            name="salary"
-                            label="Salário"
-                            type="text"
-                            id="salary"
-                            value={user.salary}
-                            onChange={handleChange}
-                        />
-                    </Grid>
-                    <Typography component="h6" variant="h6" color={'red'} fontSize={15}>
+                    </h3>
+                    <div className="containerRow">
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input '}
+                                name="office"
+                                title="Cargo"
+                                type="text"
+                                id="office"
+                            />
+                        </div>
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input '}
+                                name="sector"
+                                title="Setor"
+                                type="text"
+                                id="sector"
+                            />
+                        </div>
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input'}
+                                name="admissionDate"
+                                title="Data de admissão"
+                                type="date"
+                                id="admissionDate"
+                            />
+                        </div>
+                        <div className="itemForm">
+                            <InputGeneral
+                                required
+                                classess={'general-input '}
+                                name="salary"
+                                title="Salário"
+                                type="number"
+                                id="salary"
+                            />
+                        </div>
+                    </div>
+                    <h3 className={'errorMessage'}>
                         {errorMessage}
-                    </Typography>
+                    </h3>
 
                     <div className="containerButton">
                         <Button
-                            title="Salvar"
+                            title="Cadastrar"
                             typeButton="input-button"
                             type="submit"
                         />
                         <Button
                             title="Cancelar"
                             typeButton="input-button"
-                            onClick={handleCancel}
+                            onClick={() => handleRedirect('/employees')}
                         />
-
-
                     </div>
-                </Box>
+                </form>
             </div>
         </section >
     );
